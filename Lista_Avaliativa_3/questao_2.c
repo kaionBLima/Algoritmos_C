b620b10c5e5cd4a621c888ab43e22fadb648768b
kaionBLima <kaionbrandaolima@gmail.com>
2023-11-14 21:27:53 -0300

    lista 3

--- Lista_Avaliativa_3/questao_2.c
+++ Lista_Avaliativa_3/questao_2.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 
-double calculaMontanteFimDoMes(int mes, double aporte, double juros) {
-    return aporte * (1 + juros) * (((pow(1 + juros, mes) - 1) / juros));
+double calculaMontante(int m, double apte, double j) {
+    return apte * (1 + j) * (((pow(1 + j, m) - 1) / j));
 }
 
-void calculaEMostraMontantes(int num_meses, double aporte_mensal, double taxa_juros) {
+void calculaEMostraMontantes(int num_meses, double apte_mensal, double taxaDeJuros) {
     for (int i = 1; i <= num_meses; i++) {
-        double montante = calculaMontanteFimDoMes(i, aporte_mensal, taxa_juros);
+        double montante = calculaMontante(i, apte_mensal, taxaDeJuros);
         printf("Montante ao fim do mes %d: R$ %.2f\n", i, montante);
     }
 }
 
 int main() {
     int num_meses;
-    double aporte_mensal, taxa_juros;
+    double apte_mensal, taxaDeJuros;
 
     scanf("%d", &num_meses);
-    scanf("%lf", &aporte_mensal);
-    scanf("%lf", &taxa_juros);
+    scanf("%lf", &apte_mensal);
+    scanf("%lf", &taxaDeJuros);
 
-    calculaEMostraMontantes(num_meses, aporte_mensal, taxa_juros);
+    calculaEMostraMontantes(num_meses, apte_mensal, taxaDeJuros);
 
     return 0;
 }
