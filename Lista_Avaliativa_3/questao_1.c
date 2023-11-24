d5a6c6cc9e139a63db00d0af7d9d2eb74f7bcce9
kaionBLima <kaionbrandaolima@gmail.com>
2023-11-23 23:24:03 -0300

    qst 1 finalizada

--- Lista_Avaliativa_3/questao_1.c
+++ Lista_Avaliativa_3/questao_1.c
@@ -1,82 +1,87 @@
 #include <stdio.h>
 #include <string.h>
 
-int numerosRomanos (char represRomano) {
-    switch (represRomano){
-    case 'I': return 1;
-    case 'V': return 5;
-    case 'X': return 10;
-    case 'L': return 50;
-    case 'C': return 100;
-    case 'D': return 500;
-    case 'M': return 1000;
-    
-    default:
-    return 0;
-    }
+int num_Romanos(char representacaoRomano){
+   
+   switch (representacaoRomano)
+   {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+   }
 }
 
-    int convRomano_Decimal (char *roma) {
-        int retorno = 0;
-        for (int i = 0; roma[i]; i++) {
-            if (numerosRomanos(roma[i]) < numerosRomanos(roma[i + 1])){
-                retorno = retorno - numerosRomanos(roma[i]);
-            } else {
-                retorno += numerosRomanos(roma[i]);
-            }
+int convRomanos_Dec(char *romano){
+    int div = 0;
+    for (int i = 0; romano[i]; i++) {
+        if (num_Romanos(romano[i]) < num_Romanos(romano[i + 1])) {
+            div-= num_Romanos(romano[i]);
+        } else {
+            div+= num_Romanos(romano[i]);
         }
-        return retorno;
+    }
+
+    return div;
 }
 
-void conv_binario(int decimal, char bin[]) {
-   bin[0] = '\0';
+void convDec_Binario(int decimal, char binario[]){
+    binario[0] = '\0';
 
    while (decimal > 0) {
         char digito[2];
         sprintf(digito, "%d", decimal % 2);
-        strcat(bin, digito);
+        strcat(binario, digito);
         decimal /= 2;
     }
 
-    int tamanho = strlen(bin);
+    int tamanho = strlen(binario);
     for (int i = 0; i < tamanho / 2; i++) {
-        char temp = bin[i];
-        bin[i] = bin[tamanho - 1 - i];
-        bin[tamanho - 1 - i] = temp;
+        char temp = binario[i];
+        binario[i] = binario[tamanho - 1 - i];
+        binario[tamanho - 1 - i] = temp;
     }
 }
 
-    void conv_Hexadecimal (int decimal, char hexadec[]) {
-        int i = 0, div;
-   if (decimal == 0) {
-        printf("0\n");
-        return;
-    }
+void convDec_Hexadecimal(int decimal, char hexadecimal[]){
+   int i = 0, resto_div;
+   hexadecimal[0] = '\0';
+   
    while (decimal > 0) {
-        div = decimal % 16;
-        if (div < 10) {
-            hexadec[i] = div + '0';
+        resto_div = decimal % 16;
+        char dig[2];
+        if (resto_div < 10) {
+            sprintf(dig,"%d",resto_div);
         } else {
-            hexadec[i] = div - 10 + 'a';
+             sprintf(dig, "%c", resto_div - 10 + 'a');
         }
-
-        decimal /= 16;
+        strcat(hexadecimal, dig);
+        decimal = decimal / 16;
         i++;
     }
+    int tamanho = strlen(hexadecimal);
+    for (int i = 0; i < tamanho / 2; i++) {
+        char temp = hexadecimal[i];
+        hexadecimal[i] = hexadecimal[tamanho - 1 - i];
+        hexadecimal[tamanho - 1 - i] = temp;
+    }
 }
 
-    int main () {
-        char romano[20], binario[32], hexadecimal[100];
-        int dec;
-        scanf("%s", romano);
+int main(){
+   char romano[15],binario[32], hexadecimal[100];
+   int decimal;
+   scanf("%s",romano);
 
-        dec = convRomano_Decimal(romano);
-        conv_binario(dec, binario);
-        conv_Hexadecimal(dec,hexadecimal);
+   decimal = convRomanos_Dec(romano);
+   convDec_Binario(decimal, binario);
+   convDec_Hexadecimal(decimal,hexadecimal);
 
-        printf("%s na base 2: %s\n", romano, binario);
-        printf("%s na base 10: %d\n", romano, dec);
-        printf("%s na base 16: %s\n", romano, hexadecimal);
-
-        return 0;
-    }
+   printf("%s na base 2: %s\n",romano, binario);
+   printf("%s na base 10: %d\n",romano, decimal);
+   printf("%s na base 16: %s\n",romano, hexadecimal);
+   return 0;
+}
